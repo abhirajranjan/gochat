15a15010c74b55c272f6ff18b4dd888a6e7f64d0
abhiraj ranjan <abhirajranjan@outlook.com>
2023-08-15 03:25:10 +0530

    added env variables to parser

--- config/parser/parser.go
+++ config/parser/parser.go
@@ -2,25 +2,27 @@ package parser
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/mitchellh/mapstructure"
-	"github.com/pkg/errors"
 	"github.com/spf13/viper"
-)
-
-var (
-	ErrNoConfigFile      = errors.New("no yaml config file provided")
-	ErrInvalidConfigFile = errors.New("config file not found")
 )
 
 func Load(cfg any, configfile string) error {
 	parser := viper.New()
-	parser.SetConfigFile(configfile)
 
-	if err := parser.ReadInConfig(); err != nil {
-		return fmt.Errorf("viper.ReadIn: %w", err)
+	replacer := strings.NewReplacer(".", "_")
+	parser.SetEnvKeyReplacer(replacer)
+	parser.AutomaticEnv()
+
+	if configfile != "" {
+		parser.SetConfigFile(configfile)
+
+		if err := parser.ReadInConfig(); err != nil {
+			return fmt.Errorf("viper.ReadInConfig: %w", err)
+		}
 	}
-
+	parser.Get("sql.dsn")
 	if err := parser.Unmarshal(&cfg, mapstructureHooks()...); err != nil {
 		return fmt.Errorf("viper.Marshal: %w", err)
 	}
